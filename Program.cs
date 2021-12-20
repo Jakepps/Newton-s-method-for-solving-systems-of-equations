6a8427a0058f6893b5b15da03511b2546ba5b93b
Jakepps <nagalevsiy@mail.ru>
2021-12-20 19:28:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Newton_s_method_for_solving_systems_of_equations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
